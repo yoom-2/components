b1b947a4732b6935ad4469bfcbaa3b386c5deada
yoom-2 <99700214+yoom-2@users.noreply.github.com>
2022-06-07 00:30:33 +0900

    Update and rename Button.vue to TestBtn.vue

---
+++ src/components/TestBtn.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="btn">
+    <button class="click-btn" @click="handleBtn">Click</button>
+  </div>
+
+</template>
+<script>
+export default {
+  methods: {
+    handleBtn($event) {
+      this.$emit('click', $event.target.value);
+    },
+  }
+}
+</script>
+<style>
+  .btn {
+    width: 100%;
+    text-align: right;
+    padding-top: 10px;
+  }
+  .click-btn {
+    width: 150px;
+    height: 25px;
+    background-color: antiquewhite;
+    border: 0;
+    color: #333333;
+  }
+</style>
