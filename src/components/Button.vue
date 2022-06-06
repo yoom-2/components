dcb09432c55db9c35f05119ba33fa33156369ac6
yoom-2 <99700214+yoom-2@users.noreply.github.com>
2022-06-07 00:25:20 +0900

    Initial commit

---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="button">
+    <button>click</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Button.vue"
+}
+</script>
+
+<style>
+  button {
+    font-size: 30px;
+    border: 1px solid #000;
+  }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <h1>수정수정수정</h1>
+    <p>소연이가 수정함</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
